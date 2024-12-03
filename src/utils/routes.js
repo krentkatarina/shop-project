bcae3905dfa9838d59d8a2b691ea726a04d1a039
krentkatarina <krentkatarina58@mail.ru>
2024-12-03 23:49:41 +0300

    build

--- src/utils/routes.js
+++
@@ -1,8 +0,0 @@
-export const ROUTES = {
-    Home: '/',
-    Cart: "/cart",
-    Category: '/categories/:id',
-    Product: '/products/:id',
-    Profile: 'profile',
-    Favourites: '/favourites'
-}
